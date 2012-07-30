9898f306a6e9537eda457c55de881dcc1542344c
Kimi <kimi.zsj@gmail.com>
2012-07-30 17:09:42 +0800

    First Commit

---
+++ CopyFile/CopyFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CopyFile
+{
+    class CopyFile
+    {
+        public string Source { get; set; }
+        public string Target { get; set; }
+        public string[] Parameters { get; set; }
+        public void Copy() 
+        { 
+            
+        }
+    }
+}
